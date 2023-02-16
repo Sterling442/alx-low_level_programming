5d787df114ab9c94b1ba1a4804f81b1faeda539f
Collins Ofori Asante <casante442@gmail.com>
2023-02-16 06:04:23 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints a line of text using the ptinf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

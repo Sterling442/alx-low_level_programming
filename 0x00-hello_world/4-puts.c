62b32b76e02c2892d9231004630c55c29b09690c
Collins Ofori Asante <casante442@gmail.com>
2023-02-16 05:13:51 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line of text with the puts function
+ * the return value of this program should zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle
+			");
+	return (0);
+}

0acaae80c942f860057a869b77a628ead6d0d37c
Collins Ofori Asante <casante442@gmail.com>
2023-02-21 07:06:37 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - This function checks for alphabetic character
+ * @c: Character to be checked
+ * Return: 1 for alphabetic character or 0 for ant other character
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}

9373ee2e28f59e3a22c7adcff886585de444a275
Collins Ofori Asante <casante442@gmail.com>
2023-02-23 05:23:20 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - This function checks for uppercase characters
+ * @x: Number to be tested
+ * Return: 1 for uppercase or 0 for any other
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains files for 0x04-more functions nested loops project

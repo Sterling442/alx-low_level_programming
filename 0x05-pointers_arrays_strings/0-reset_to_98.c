38e75cbb1e3d6a539b6dd92fcdb8a2a50d87df37
Collins Ofori Asante <casante442@gmail.com>
2023-02-27 07:52:49 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - This func takes a pointer to an int an updates it to 98
+ * @n: for input
+ * Return: empty
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
